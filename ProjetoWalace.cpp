3daa172fe99002c5048031af383c6b26e8ff9124
Gabriel Ferrreira <gabrieldouble007@gmail.com>
2026-04-01 12:33:33 -0300

    Criacao da struct da lista de alunos(lista_aluno) e funcao para iniciar a lista

--- ProjetoWalace.cpp
+++ ProjetoWalace.cpp
@@ -1,17 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 60
 
-typedef struct Lista{ // Struct da lista encadeada das disciplinas
-    char disciplina[20]; // aqui o nome da disciplina
+// ~~~~~~~~~~STRUCTS~~~~~~~~~~~~~~~~
+typedef struct Lista_disciplina{ // Struct da lista encadeada das disciplinas
+    char disciplina[20];
+    float nota; 
     struct Lista * proximo;// e um ponteiro que apontaria para a proxima disciplina
-}lista;
+}lista_disciplina;
 
-typedef struct Aluno{ // Struct de Aluno que guarda suas disciplinas
-    int RGM;
-    Lista * disciplinas_lista; // um ponteiro que irá armazenar uma lista encadeada do tipo struct Lista que contém as disciplinas desse aluno
+typedef struct Aluno{ // Struct de Aluno que seu RGM e suas disciplinas
+    char RGM[15];
+    lista_disciplina * disciplinas_lista; // um ponteiro que irá armazenar uma lista encadeada do tipo struct Lista que contém as disciplinas desse aluno
 }aluno;
 
+typedef struct Lista_aluno{ // Struct que guarda a lista sequencial para armazenar os alunos e o ULTIMO aluno que foi adicionado a lista
+    aluno alunos[MAX];
+    int ultimo_aluno;// ultimo aluno adicionado 
+}lista_aluno;
+
+// ~~~~~~~~~~FUNÇÕES~~~~~~~~~~~~~~~~
+
+lista_aluno criar_listasequencial(){ // inicializa a lista que guarda os alunos
+    lista_aluno listasequencial; // inicializa a lista
+    listasequencial.ultimo_aluno = -1; // define o ultimo aluno como -1(significa que a lista está vazia)
+
+    return listasequencial; // retorna lista criada
+}
+
 int main(){
+    lista_aluno lista_alunos = criar_listasequencial(); // variável guarda o retorno da função
 
     return 0;
 }
